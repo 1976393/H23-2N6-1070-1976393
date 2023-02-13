5198505462b1f813870dfe5956811f8af72af0f3
Thibault William <1976393@cegepmontpetit.ca>
2023-02-13 09:43:13 -0500

    FTC Exercice 3

--- R07 Exercices Depart/R07_ex2_Invitations.py
+++ R07 Exercices Depart/R07_ex2_Invitations.py
@@ -20,8 +20,12 @@ print("Bonjour Thomas, \n Je veux t'inviter à venir chez moi demain vers 18h00.
 
 # Q1 A:  Faites une fonction pour inviter tout le monde, sans duplication du code.
 #        Cette fonction prendra une liste d'amis en paramètres                                      #
-
-  
+liste = ["Marc","Pierre","Lucie","Achraf","Claude","Ilyan","Maria","Thomas"]
+def invitation(liste=[]):
+    for nom in liste:
+        print(f"Bonjour {nom}, \n Je veux t'inviter à venir chez moi demain vers 18h00. Cela t'intéresse?")
+invitation(liste)
+      
   
   
   
@@ -31,15 +35,19 @@ print("Bonjour Thomas, \n Je veux t'inviter à venir chez moi demain vers 18h00.
 #         Puis faites l'appel de la fonction en lui passant en paramètre votre liste d'amis
 
 
-
-
+    
 
 # Q2 A:   Fonction pour inviter tout le monde, dans un lieu, un jour et une heure qui varient, sans duplication du code
 #         Appelez cette fonction invitation2
-
+liste = ["Marc","Pierre","Lucie","Achraf","Claude","Ilyan","Maria","Thomas"]
+def invitation2(liste,lieu,jour,heure):
+    for nom in liste:
+        print(f"Bonjour {nom}, \n Je veux t'inviter à venir {lieu} {jour} vers {heure}. Cela t'intéresse?")
+invitation2(liste,"au parc","vendredi","à 15h00")
 
 # Q2 B:  Appel de la fonction invitation2 pour inviter Sylvain et Nathalie au café étudiant, vendredi, vers 13h00
 
+invitation2(liste[0],"au café étudiant", "vemdredi", "à 13h00")
 
 # Q2 C:  Appel de la fonction invitation2 pour inviter Sylvain, Nathalie et Paul chez moi pour souper, samedi vers 17h00
 
