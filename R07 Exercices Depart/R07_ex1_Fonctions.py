95cb9406a725bb8c19b681164a92f6a851fc2abe
Thibault William <1976393@cegepmontpetit.ca>
2023-02-13 09:13:51 -0500

    FTC Exercice 1

--- R07 Exercices Depart/R07_ex1_Fonctions.py
+++ R07 Exercices Depart/R07_ex1_Fonctions.py
@@ -4,10 +4,12 @@
 #        Faites une fonction qui va imprimer "Bonjour" 
 #        Appelez-la bonjour
 
+def bonjour():
+    print("Bonjour")
 
 
 # Q1 B:   Appel de la fonction 
-
+bonjour()
 
 
 
@@ -15,16 +17,17 @@
 # Q2 A:   Définir une fonction avec paramètre
 #         Faites une fonction qui va imprimer "Bonjour XXXXXX" où XXXXXX est le nom passé en paramètre lors de l'appel de cette fonction
 #         Appelez-la bonjour_toi
-
+def bonjour_toi(nom):
+    print(f"Bonjour {nom}")
  
  
 
 # Q2 B:  Appel de la fonction bonjour_toi avec en paramètre le nom de mon ami Pierre
-
+bonjour_toi("Pierre")
 
 
 # Q2 C:  Appel de la fonction bonjour_toi avec en paramètre le nom de mon amie Sylvie
-
+bonjour_toi("Sylvie")
 
 
 
@@ -32,12 +35,16 @@
 # Q3 A:  Définir une fonction sans paramètre mais qui va retourner une valeur de retour      #
 #        Faites une fonction qui va imprimer "Bonjour" et qui va retourner le message "Cela va?"
 #        Appelez-la cela_va
+def cela_va():
+    print("Bonjour")
+    return("Cela va?")
 
 
 
 
 # Q3 B:   Appel de la fonction et capture de la réponse
-
+a = cela_va()
+print(a)
 
 
 
@@ -47,7 +54,12 @@
 # 		 et qui va retourner le message "Je suis libre ce soir, et toi?"
 #        Appelez-la cela_va_bien
 
+def cela_va_bien(nom):
+    print(f"Bonjour {nom}")
+    return("Je suis libre ce soir, et toi?")
 
+b = cela_va_bien("Bob")
+print(b)
 
 
 
