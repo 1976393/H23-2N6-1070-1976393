ae99c3e9a70527a52e22442d1bb3eb9e55b26344
Thibault William <1976393@cegepmontpetit.ca>
2023-02-20 10:04:38 -0500

    FTC Exercie 1

--- Rencontre 9/R09 Exercices Depart/Ex1_requests.py
+++ Rencontre 9/R09 Exercices Depart/Ex1_requests.py
@@ -3,6 +3,7 @@
 # Dans le terminal              pip install requests
 
 # fakerapi.it/en
+from textwrap import indent
 import requests
 import json
 
@@ -14,7 +15,9 @@ BASE_URL = 'https://fakerapi.it/api/v1'
 
 # Q1)   faire une demande de 1 produit1 (GET/products?_quantity=1) et conservez le résultat dans une variable
 
+answer = requests.get(f'{BASE_URL}/products?_quantity=1')
 
+print(answer)
 
 
 # json.dump permet d'ajouter des arguments
@@ -22,12 +25,13 @@ BASE_URL = 'https://fakerapi.it/api/v1'
 # et que ce soit plus facile à comprendre la structure de la réponse
 
 # Q2)  faites un dumps de la réponse obtenue en Q1 avec un niveau d'indentation de 4  et conservez le résultat dans une variable
-
-
+texte_string = json.dumps(answer.json(),indent=4)
+print(texte_string)
 
 # Q3)  Le résultat obtenu en Q2 est un string et doit être changé en un dictionnaire avec json.load  et conservez le résultat dans une variable
 # using json.loads() method
-
+texte_indent = json.loads(texte_string)
+print(texte_indent)
 
 #Si je regarde seulement ce qui m’intéresse
 #dict_resultat = {
@@ -56,20 +60,28 @@ BASE_URL = 'https://fakerapi.it/api/v1'
 #  Pour le moment, avec un produit, je peut obtenir le 'price' du produit et vérifier s'il est dans la catégorie 3.
 
 # Q4)  Obtenez le premier élément du dictionnaire avec la clé 'data' dans une variable
-
+data = json.loads(requests.get(f'{BASE_URL}/products?quantity=1').text)
+print("_"*80)
+print(data['data'][0])
+#print(json.dumps(data,indent=4))
 
 #  Donc on obtient quelque chose comme
 # {'id': 1, 'name': 'Blanditiis aut in quia omnis.', 'net_price': 2.15, 'taxes': 22, 'price': '2.62', 'categories': [2, 3, 5]}
 
-
 #  Q5) Obtenez le prix et changez le en un float
-
+prix = float(data['data'][0]["price"])
+print(prix)
 
 #  Q6)  Utilisez in pour vérifier si 3 fait partie des categories du dictionnaire des categories du produit
-
+#for num in data["data"][0]["categories"]:
+if 3 in data["data"][0]["categories"]:
+    print("true")
 #  Q7)  Imprimez le prix et si le produit est dans la catégorie 3 ou non
 #       Quelque chose comme: "Le produit a le prix de 3945.01 et on peut savoir s'il est dans la catégorie 3: False"
-
+if 3 in data["data"][0]["categories"]:
+    print(f'Le produit a le prix de {float(data["data"][0]["price"])} et on peut savoir s\'il est dans la catégorie 3: True')
+else:
+    print(f'Le produit a le prix de {float(data["data"][0]["price"])} et on peut savoir s\'il est dans la catégorie 3 : False')
 
 
 
