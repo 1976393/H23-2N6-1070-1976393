cd92bae7a6a729a3f6bed746b920a7a385562162
1976393 <1976393@cegepmontpetit.ca>
2023-02-28 15:30:44 -0500

    Comment

--- Rencontre 11/s1_lire_csv_ventes.py
+++ Rencontre 11/s1_lire_csv_ventes.py
@@ -16,22 +16,37 @@ os.chdir(os.path.dirname(__file__)) # Cette ligne fait que l'exécution du scrip
 def csv_data_extract():
     with open("data_ventes.csv","r",encoding="utf-8") as csv_file:
         csv_reader = csv.reader(csv_file)
+        #Saut de 5 lignes
         for index in range(5):
             next(csv_reader)
         ls_clients_raw = []
+        #Ajout des lignes du csv non modifiees à ls_clients_raw et création de la liste finale ls_clients
         for line in csv_reader:
             ls_clients_raw.append(line)
         ls_clients = []
+        #Pour chaque liste de ls_clients_raw, 
+        #- Creation de dictionnaires sans informations
+        #- Update le dictionnaire avec les informations des listes
+        #- Création d'une liste à mettre les prochaines informations nommé liste_commande
+        #- Compteur mit à 1 (productId débute à 1)
         for ls in ls_clients_raw:
             dict_clients = {"userId":"","nom":"","prenom":"","commande":[{"productId":"","quantite":[{}]}]}
             dict_clients.update({"userId": ls[0],"nom":ls[1],"prenom":ls[2]})
             liste_commande = []
             cpt = 1
+            
+            #De 3 à la fin de la liste,
+            #- Creation de dictionnaires sans informations 
+            #- Update les dictionnaires avec le compteur et l'info des listes
+            #- Ajout du dictionnaire à la fin de la liste
+            #- Augmentation du compteur
             for info in ls[3:]:
                 dict = {"productId":"","quantite":""}
                 dict.update({"productId":cpt,"quantite":info})
                 liste_commande.append(dict)
                 cpt+=1
+            #Ajout des informations de commande au dictionnaire de client
+            #Ajout du dictionnaire client à la liste d'informations des client; ls_clients
             dict_clients.update({"commande":liste_commande})
             ls_clients.append(dict_clients) 
         #print(ls_clients)
--- Rencontre 11/s2_req_produit_api.py
+++ Rencontre 11/s2_req_produit_api.py
@@ -14,21 +14,33 @@ url = "https://fakestoreapi.com/"
 
 import s1_lire_csv_ventes as s1
 
+#Résultat du script 1 mis dans info
 info = []
 info = s1.csv_data_extract()
 
 
 
 def addition_info():
+    #Requêtes des informations du API mises dans data_api
     data_api_req = requests.get(f'{url}products?limit=20')
     data_api_json = data_api_req.json()
     data_api_dumps = json.dumps(data_api_json,indent=4)
     data_api = json.loads(data_api_dumps)
     # print(data_api)
     
+    #Passement au travers de chaque ligne de info
     for ls in range(len(info)):
+        #Passement au travers de chaque dictionnaire de ["commande"]
         for dict in info[ls]["commande"]:
+            #Passement au travers de chaque ligne du data de l'api
             for ls_api in data_api:
+                
+                #Si le productId de info est le même que le id des informations de l'api
+                #- création de dictionnaires sans informations
+                #- update des dictionnaires avec le prix et la categorie de l'id qui est le même
+                #- update du dictionnaire commande dans la variable info à la position du id 
+                # dans la liste de dictionnaire commande -1 (position du id dans la liste est 1 de moins que le id)
+                
                 if dict["productId"] == ls_api["id"]:
                     price = {"prix":""}
                     categorie = {"categorie":""}
@@ -36,18 +48,7 @@ def addition_info():
                     categorie.update({"categorie":ls_api["category"]})
                     info[ls]["commande"][dict["productId"]-1].update(price)
                     info[ls]["commande"][dict["productId"]-1].update(categorie)
-    indented_data = json.dumps(info,indent=4)
-    print(indented_data)
-            
-            
-            
-            
-            # for id in range(len(dict)):
-                
-            # for id in ls["commande"][index]:
-            #     print(id)
-                #print(ls["commande"][index]["productId"])
-            # for id in ls_commande:
-                
-
-addition_info()
+    # indented_data = json.dumps(info,indent=4)
+    # print(indented_data)
+    return(info)
+# addition_info()
